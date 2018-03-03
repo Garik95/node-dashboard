567135c14d051ecc1e39f00bab3105c73f55c01d
Garik Nurmurodoff <garikn95@gmail.com>
2018-03-03 14:25:15 -0500

    Configured Login Signup Index(Profile)Logout routes

--- app/routes.js
+++ app/routes.js
@@ -4,12 +4,12 @@ module.exports = function(app, passport) {
 
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
-        res.render('index.ejs');
+        res.render('template/login.ejs',{message:0});
     });
 
     // PROFILE SECTION =========================
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
+        res.render('template/index.ejs', {
             user : req.user
         });
     });
@@ -28,7 +28,7 @@ module.exports = function(app, passport) {
         // LOGIN ===============================
         // show the login form
         app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
+            res.render('template/login.ejs', { message: req.flash('loginMessage') });
         });
 
         // process the login form
@@ -41,7 +41,7 @@ module.exports = function(app, passport) {
         // SIGNUP =================================
         // show the signup form
         app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
+            res.render('template/signup.ejs', { message: req.flash('signupMessage') });
         });
 
         // process the signup form
