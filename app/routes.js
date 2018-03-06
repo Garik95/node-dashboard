bf2fe746729a9b1bbb1bea93f4aa0d9a178b341f
Garik Nurmurodoff <garikn95@gmail.com>
2018-03-06 10:54:23 +0500

    auth route test

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,8 @@ module.exports = function(app, passport) {
 
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
-        res.render('index.ejs');
+        if(req.isAuthenticated){res.redirect('/profile');}
+        else res.render('index.ejs');
     });
 
     // PROFILE SECTION =========================
